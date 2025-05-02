693a4c05036691fbbbfdd591eab5dcbb0ba258cd
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-02 20:01:34 +0330

    Add StudentModel class with prototype scope and basic properties

---
+++ src/main/java/com/parsa/security/model/StudentModel.java
@@ -0,0 +1,48 @@
+package com.parsa.security.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class StudentModel {
+    private int id;
+    private String name;
+    private int marks;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public StudentModel(int id, String name, int marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentModel [id=" + id + ", name=" + name + ", marks=" + marks + "]";
+    }
+
+}
