7c5b015e68dfc03fad1b08c067e5c55808cf6a13
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-02 20:07:23 +0330

    Refactor StudentController to initialize student list and improve formatting

--- src/main/java/com/parsa/security/controller/StudentController.java
+++ src/main/java/com/parsa/security/controller/StudentController.java
@@ -1,8 +1,31 @@
 package com.parsa.security.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.parsa.security.model.StudentModel;
 
 @RestController
 public class StudentController {
-    
+
+    private List<StudentModel> students = new ArrayList<StudentModel>(List.of(
+            new StudentModel(1, "Ali", 90),
+            new StudentModel(2, "Reza", 85),
+            new StudentModel(3, "Sara", 95)));
+
+    @GetMapping("/students")
+    public List<StudentModel> getStudents() {
+        return students;
+    }
+
+    @PostMapping("/addstudent")
+    public void addStudent(@RequestBody StudentModel student
+    ) {
+        students.add(student);
+    }
 }
