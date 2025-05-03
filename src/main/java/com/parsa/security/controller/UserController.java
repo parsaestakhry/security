75889818b825a3db49c66e805d8070854c7c9743
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-03 19:47:57 +0330

    Add UserController for user registration endpoint

---
+++ src/main/java/com/parsa/security/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.parsa.security.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.parsa.security.model.Users;
+import com.parsa.security.service.UserService;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/register")
+    public Users register(@RequestBody Users user) {
+        return service.register(user);
+    }
+}
