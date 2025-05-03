b68302b5c5cbddf4b2bf8d99725316e1b4ab5be4
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-03 19:57:45 +0330

    Replace NoOpPasswordEncoder with BCryptPasswordEncoder for improved password security

--- src/main/java/com/parsa/security/config/SecurityConfig.java
+++ src/main/java/com/parsa/security/config/SecurityConfig.java
@@ -10,13 +10,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
 
     @Autowired
     private UserDetailsService userDetailsService;
@@ -38,7 +37,7 @@ public class SecurityConfig {
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         // setting no password encoder
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         provider.setUserDetailsService(userDetailsService);
         return provider;
     }
