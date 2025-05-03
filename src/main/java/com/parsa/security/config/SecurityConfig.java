663c6cd4ac6a6e702f3012c9e848d19aa1559d5d
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-03 17:46:18 +0330

    Refactor SecurityConfig to include UserDetailsService and improve authentication provider setup

--- src/main/java/com/parsa/security/config/SecurityConfig.java
+++ src/main/java/com/parsa/security/config/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.parsa.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,14 +34,29 @@ public class SecurityConfig {
                 .build();
     }
 
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        // setting no password encoder
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+
     // this is not the way to approach if we want to use a database based security
 
-     /* @Bean
-    public UserDetailsService userDetailsService() {
-
-        UserDetails user1 = User.withDefaultPasswordEncoder().username("Venom").password("password").build();
-        UserDetails user2 = User.withDefaultPasswordEncoder().username("Kaz").password("password").build();
-
-        return new InMemoryUserDetailsManager(user1, user2); 
-    } */ 
+    /*
+     * @Bean
+     * public UserDetailsService userDetailsService() {
+     * 
+     * UserDetails user1 =
+     * User.withDefaultPasswordEncoder().username("Venom").password("password").
+     * build();
+     * UserDetails user2 =
+     * User.withDefaultPasswordEncoder().username("Kaz").password("password").build(
+     * );
+     * 
+     * return new InMemoryUserDetailsManager(user1, user2);
+     * }
+     */
 }
