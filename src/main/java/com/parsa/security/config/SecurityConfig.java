5c3aa221e46df5e57afb7eb5956b9d55e9758cf0
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-03 17:00:37 +0330

    Add UserDetailsService bean for in-memory user authentication

--- src/main/java/com/parsa/security/config/SecurityConfig.java
+++ src/main/java/com/parsa/security/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +27,15 @@ public class SecurityConfig {
                 // stateless http
                 sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User.withDefaultPasswordEncoder().username("Venom").password("password").build();
+        UserDetails user2 = User.withDefaultPasswordEncoder().username("Kaz").password("password").build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
