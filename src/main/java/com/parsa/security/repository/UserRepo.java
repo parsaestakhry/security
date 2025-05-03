4c7e906d27975f934f6f718d61471a07a69eb236
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-03 19:17:02 +0330

    Add @Repository annotation to UserRepo interface for clarity

--- src/main/java/com/parsa/security/repository/UserRepo.java
+++ src/main/java/com/parsa/security/repository/UserRepo.java
@@ -1,9 +1,13 @@
 package com.parsa.security.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.parsa.security.model.Users;
 
-public interface UserRepo extends JpaRepository<Users, Integer>  {
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
 
 }
