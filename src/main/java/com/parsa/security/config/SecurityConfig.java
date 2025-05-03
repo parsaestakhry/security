cd1cd87f7139fc725f9202d125d2fb100f469d05
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-03 13:41:44 +0330

    Add form login configuration to SecurityFilterChain

--- src/main/java/com/parsa/security/config/SecurityConfig.java
+++ src/main/java/com/parsa/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.parsa.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,13 +10,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(customizer -> customizer.disable());
         http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
         return http.build();
     }
 }
