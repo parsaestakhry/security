a6683072c6fff2d8ad5e128c776519424f69e0a3
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-03 19:29:02 +0330

    Implement getAuthorities method in UserPrincipal to return user roles

--- src/main/java/com/parsa/security/model/UserPrincipal.java
+++ src/main/java/com/parsa/security/model/UserPrincipal.java
@@ -1,8 +1,10 @@
 package com.parsa.security.model;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class UserPrincipal implements UserDetails {
@@ -15,7 +17,7 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+        return Collections.singleton(new SimpleGrantedAuthority("user"));
     }
 
     @Override
