5f7f8aa27b403be067c23bd14c3fa46a37a2e896
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-03 19:17:07 +0330

    Implement user retrieval in MyUserDetailsService and remove unimplemented method stub

--- src/main/java/com/parsa/security/service/MyUserDetailsService.java
+++ src/main/java/com/parsa/security/service/MyUserDetailsService.java
@@ -1,18 +1,31 @@
 package com.parsa.security.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.parsa.security.model.Users;
+import com.parsa.security.repository.UserRepo;
+
 
 @Service
-public class MyUserDetailsService implements UserDetailsService{
+public class MyUserDetailsService implements UserDetailsService {
+    
+
+    @Autowired
+    private UserRepo repo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+        Users user = repo.findByUsername(username);
+        if (user == null) {
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("User not found");
+        } else {
+            return null; 
+        }
     }
     
 }
