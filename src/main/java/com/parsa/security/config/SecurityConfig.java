03aaef9cd6b874e1690fb35e59802827783777fb
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-03 13:53:42 +0330

    Refactor security filter chain configuration for improved readability and maintainability

--- src/main/java/com/parsa/security/config/SecurityConfig.java
+++ src/main/java/com/parsa/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,11 +15,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(customizer -> customizer.disable());
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        http.formLogin(Customizer.withDefaults());
-        // enable login from outside the spring login form
-        http.httpBasic(Customizer.withDefaults());
-        return http.build();
+        return http.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                // http.formLogin(Customizer.withDefaults());
+                // enable login from outside the spring login form
+                .httpBasic(Customizer.withDefaults()).
+                // stateless http
+                sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
