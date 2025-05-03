81762f36b0a7b850de94bce3e46fb8dde95c3ce0
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-03 19:57:51 +0330

    Add BCryptPasswordEncoder for password encryption in user registration

--- src/main/java/com/parsa/security/controller/UserController.java
+++ src/main/java/com/parsa/security/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.parsa.security.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,11 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     @PostMapping("/register")
     public Users register(@RequestBody Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return service.register(user);
     }
 }
