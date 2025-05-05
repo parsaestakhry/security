84f0c1190a1d285e9353e5e9ef744b84a9b28189
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-05 16:44:53 +0330

    Implement user verification method in UserService for authentication and token generation

--- src/main/java/com/parsa/security/service/UserService.java
+++ src/main/java/com/parsa/security/service/UserService.java
@@ -1,6 +1,8 @@
 package com.parsa.security.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import com.parsa.security.model.Users;
@@ -12,7 +14,23 @@ public class UserService {
     @Autowired
     private UserRepo repo;
 
+    @Autowired
+    AuthenticationManager manager;
+
+    @Autowired
+    private JwtService jwtService;
+
     public Users register(Users user) {
         return repo.save(user);
+    }
+
+    public String verify(Users user) {
+        org.springframework.security.core.Authentication authentication = manager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken();
+        }
+
+        return "fail";
     }
 }
