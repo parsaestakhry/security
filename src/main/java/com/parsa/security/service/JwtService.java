6531c4762e7317dd357efc5db947f25c48824d0c
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-05-05 17:01:31 +0330

    Refactor JwtService to generate secure JWT tokens with dynamic secret key

--- src/main/java/com/parsa/security/service/JwtService.java
+++ src/main/java/com/parsa/security/service/JwtService.java
@@ -1,12 +1,53 @@
 package com.parsa.security.service;
 
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 
-    public String generateToken() {
-        return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZlbm9tIiwicGFzc3dvcmQiOiJ2ZW5vbSIsImlkIjo1fQ.O3owVOG7NSAchPvJkhEF8ryKmJdZ3lTxUD7slL8NSBY";
+    private String secretKEY;
+
+    public JwtService() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey = keyGen.generateKey();
+            secretKEY = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> calaims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims()
+                .add(calaims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() +  24L * 60 * 60 * 1000))
+                .and()
+                .signWith(getKey())
+                .compact();
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 }
